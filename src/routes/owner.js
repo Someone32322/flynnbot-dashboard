4fea2472a5642083cdbd6c68ed207bc9f6fe7424
Someone <lone89487@gmail.com>
2026-05-10 17:35:47 +0930

    your message

--- src/routes/owner.js
+++ src/routes/owner.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 const path = require('path');
-const { ensureAuthenticated, ensureIsOwner } = require('../middleware');
+const { ensureAuthenticated, ensureIsOwner } = require('../middleware/auth.js');
 
 const changelogFilePath = path.join(__dirname, '../../data/changelog.json');
 
