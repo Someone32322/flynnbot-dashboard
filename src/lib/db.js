45076a8d634e41a4620e4c00e6b0b12eaa7652d7
Someone <lone89487@gmail.com>
2026-05-10 20:07:35 +0930

    diag: log actual MongoDB database name on connect

--- src/lib/db.js
+++ src/lib/db.js
@@ -8,7 +8,8 @@ async function connectDb() {
   if (!uri) throw new Error('MONGO_URI is not set in the dashboard .env');
   await mongoose.connect(uri);
   connected = true;
-  console.log('[Dashboard] Connected to MongoDB');
+  const dbName = mongoose.connection.db.databaseName;
+  console.log(`[Dashboard] Connected to MongoDB | DB: ${dbName}`);
 }
 
 module.exports = { connectDb };
